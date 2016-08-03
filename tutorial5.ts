0751099f7df0952501aad1a92d8cbb8bd9ac9767
Vishwas <gopinav5@gmail.com>
2016-08-03 18:44:46 +0530

    Commit tutorial5

---
+++ tutorial5.ts
@@ -0,0 +1,9 @@
+function greetPerson(name){
+    if(name === "Chandler"){
+        var greet = "Hello Chandler";
+    }else{
+        var greet = "Hi there";
+    }
+    console.log(greet);
+}
+greetPerson("Chandler");
