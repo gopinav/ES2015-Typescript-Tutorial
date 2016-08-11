71a93195df62aacac3525ad4f7039e78401de813
Vishwas <gopinav5@gmail.com>
2016-08-11 05:57:10 +0530

    Commit tutorial18.ts

---
+++ tutorial18.ts
@@ -0,0 +1,17 @@
+//let employee = ["Chandler", "Bing", "Female"];
+//let [fname, lname, gender="Male"] = employee;
+let employee = {
+    fname: "Chandler",
+    lname: "Bing",
+    gender: "Male"
+};
+
+let {fname, lname, gender} = employee;
+
+
+
+console.log(fname);
+console.log(lname);
+console.log(gender);
+
+
