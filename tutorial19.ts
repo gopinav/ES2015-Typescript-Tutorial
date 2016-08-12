d2f52ba512d4d988e04af5f5496b05ee8cdfc7d0
Vishwas <gopinav5@gmail.com>
2016-08-12 06:00:39 +0530

    Commit tutorial19

---
+++ tutorial19.ts
@@ -0,0 +1,10 @@
+
+let user = "Chandler";
+
+let greet = `Welcome 'single' "double" ${user} to ES2015
+            This is the second line.
+               Third and so          on.
+
+`;
+
+console.log(greet);
