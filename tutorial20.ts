fd8884715d7edd703d34684e0810d418bf5a4b45
Vishwas <gopinav5@gmail.com>
2016-08-13 15:19:04 +0530

    commit tutorial20.ts

---
+++ tutorial20.ts
@@ -0,0 +1,19 @@
+
+let colors = ['Red', 'Blue', 'Green'];
+
+for(let index in colors){
+    console.log(colors[index]);
+    
+}
+
+for(let color of colors){
+    console.log(color);
+    
+}
+
+let letters = "ABC";
+
+for(let letter of letters){
+    console.log(letter);
+    
+}
