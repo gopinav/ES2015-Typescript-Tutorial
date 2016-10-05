90694685381d85e011e6d35a02152a8acec858f2
Vishwas <gopinav5@gmail.com>
2016-10-05 05:55:07 +0530

    Commit Tutorial46

---
+++ tutorial46.ts
@@ -0,0 +1,11 @@
+let strArr1: string[] = ['Hello','world'];
+let strArr2: Array<string> = ['Hello', 'world'];
+
+let anyArr: any[] = ['Hello', 10, true];
+
+let myTuple: [string, number] = ["Hi", 10];
+console.log(myTuple[0]);
+console.log(myTuple[1]);
+myTuple[2] = 100;
+console.log(myTuple[2]);
+
