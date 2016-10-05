76230d2048d3c49f6ee1c119ceed758e0598c7b5
Vishwas <gopinav5@gmail.com>
2016-10-05 05:55:23 +0530

    Commit Tutorial48

---
+++ tutorial48.ts
@@ -0,0 +1,16 @@
+interface Person{
+    fname: string;
+    lname: string;
+    age?: number;
+}
+
+let employee1: Person = {
+    fname:"Chandler",
+    lname:"Bing",
+    age:30
+}
+
+let employee2: Person ={
+    fname:"Ross",
+    lname:"Geller"
+}
